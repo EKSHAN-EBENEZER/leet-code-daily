1f4b7369c9b4b505441242c88054d251a052a59d
Ekshan Ebenezer <69078104+EKSHAN-EBENEZER@users.noreply.github.com>
2024-06-17 21:14:05 +0530

    Create 633. Sum of Square Numbers.py

---
+++ 633. Sum of Square Numbers.py
@@ -0,0 +1,34 @@
+'''
+Given a non-negative integer c, decide whether there're two integers a and b such that a2 + b2 = c.
+
+ 
+
+Example 1:
+
+Input: c = 5
+Output: true
+Explanation: 1 * 1 + 2 * 2 = 5
+Example 2:
+
+Input: c = 3
+Output: false
+ 
+
+Constraints:
+
+0 <= c <= 231 - 1
+'''
+
+
+
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        left, right = 0, int(math.sqrt(c))
+        while left <= right:
+            if left * left + right * right == c:
+                return True
+            elif left * left + right * right > c:
+                right -= 1
+            else:
+                left += 1
+        return False
