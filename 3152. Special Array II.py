5a7656d1af7b62afdbb1514b5ec9e92b24e37995
Ekshan Ebenezer <69078104+EKSHAN-EBENEZER@users.noreply.github.com>
2024-12-09 23:49:05 +0530

    Create 3152. Special Array II.py

---
+++ 3152. Special Array II.py
@@ -0,0 +1,64 @@
+'''
+An array is considered special if every pair of its adjacent elements contains two numbers with different parity.
+
+You are given an array of integer nums and a 2D integer matrix queries, where for queries[i] = [fromi, toi] your task is to check that 
+subarray
+ nums[fromi..toi] is special or not.
+
+Return an array of booleans answer such that answer[i] is true if nums[fromi..toi] is special.
+
+ 
+
+Example 1:
+
+Input: nums = [3,4,1,2,6], queries = [[0,4]]
+
+Output: [false]
+
+Explanation:
+
+The subarray is [3,4,1,2,6]. 2 and 6 are both even.
+
+Example 2:
+
+Input: nums = [4,3,1,6], queries = [[0,2],[2,3]]
+
+Output: [false,true]
+
+Explanation:
+
+The subarray is [4,3,1]. 3 and 1 are both odd. So the answer to this query is false.
+The subarray is [1,6]. There is only one pair: (1,6) and it contains numbers with different parity. So the answer to this query is true.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 105
+1 <= queries.length <= 105
+queries[i].length == 2
+0 <= queries[i][0] <= queries[i][1] <= nums.length - 1
+'''
+
+
+
+
+
+
+class Solution:
+    def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
+        n=len(nums)
+        prev=nums[0]&1
+        sameParity=[0]*n
+        j=0
+        for i in range(n):
+            x=nums[i]&1
+            if x==prev: j+=1
+            sameParity[i]=j
+            prev=x
+        m=len(queries)
+        ans=[False]*m
+        for i, (s, t) in enumerate(queries):
+            ans[i]=(sameParity[s]==sameParity[t])
+        return ans
+        
