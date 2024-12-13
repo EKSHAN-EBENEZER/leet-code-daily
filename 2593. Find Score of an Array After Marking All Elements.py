59c5892a9a1499ccf0ddf66832bc76e8a2d32c0f
Ekshan Ebenezer <69078104+EKSHAN-EBENEZER@users.noreply.github.com>
2024-12-13 22:10:52 +0530

    Create 2593. Find Score of an Array After Marking All Elements.py

---
+++ 2593. Find Score of an Array After Marking All Elements.py
@@ -0,0 +1,64 @@
+'''
+You are given an array nums consisting of positive integers.
+
+Starting with score = 0, apply the following algorithm:
+
+Choose the smallest integer of the array that is not marked. If there is a tie, choose the one with the smallest index.
+Add the value of the chosen integer to score.
+Mark the chosen element and its two adjacent elements if they exist.
+Repeat until all the array elements are marked.
+Return the score you get after applying the above algorithm.
+
+ 
+
+Example 1:
+
+Input: nums = [2,1,3,4,5,2]
+Output: 7
+Explanation: We mark the elements as follows:
+- 1 is the smallest unmarked element, so we mark it and its two adjacent elements: [2,1,3,4,5,2].
+- 2 is the smallest unmarked element, so we mark it and its left adjacent element: [2,1,3,4,5,2].
+- 4 is the only remaining unmarked element, so we mark it: [2,1,3,4,5,2].
+Our score is 1 + 2 + 4 = 7.
+Example 2:
+
+Input: nums = [2,3,5,1,3,2]
+Output: 5
+Explanation: We mark the elements as follows:
+- 1 is the smallest unmarked element, so we mark it and its two adjacent elements: [2,3,5,1,3,2].
+- 2 is the smallest unmarked element, since there are two of them, we choose the left-most one, so we mark the one at index 0 and its right adjacent element: [2,3,5,1,3,2].
+- 2 is the only remaining unmarked element, so we mark it: [2,3,5,1,3,2].
+Our score is 1 + 2 + 2 = 5.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 106
+'''
+
+
+
+
+from heapq import heappush, heappop
+
+class Solution:
+    def findScore(self, nums):
+        n = len(nums)
+        pq = []
+        for i, val in enumerate(nums):
+            heappush(pq, (val, i))
+        
+        sum_ = 0
+        vis = [False] * n
+        while pq:
+            val, ind = heappop(pq)
+            if vis[ind]:
+                continue
+            sum_ += val
+            vis[ind] = True
+            if ind - 1 >= 0:
+                vis[ind - 1] = True
+            if ind + 1 < n:
+                vis[ind + 1] = True
+        return sum_
