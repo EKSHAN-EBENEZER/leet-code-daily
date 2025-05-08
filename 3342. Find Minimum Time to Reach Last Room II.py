7617a761fc672d9e8bdd38299caf7477a5d52c6c
Ekshan Ebenezer <69078104+EKSHAN-EBENEZER@users.noreply.github.com>
2025-05-08 21:33:21 +0530

    Create 3342. Find Minimum Time to Reach Last Room II.py

---
+++ 3342. Find Minimum Time to Reach Last Room II.py
@@ -0,0 +1,78 @@
+'''
+There is a dungeon with n x m rooms arranged as a grid.
+
+You are given a 2D array moveTime of size n x m, where moveTime[i][j] represents the minimum time in seconds when you can start moving to that room. You start from the room (0, 0) at time t = 0 and can move to an adjacent room. Moving between adjacent rooms takes one second for one move and two seconds for the next, alternating between the two.
+
+Return the minimum time to reach the room (n - 1, m - 1).
+
+Two rooms are adjacent if they share a common wall, either horizontally or vertically.
+
+ 
+
+Example 1:
+
+Input: moveTime = [[0,4],[4,4]]
+
+Output: 7
+
+Explanation:
+
+The minimum time required is 7 seconds.
+
+At time t == 4, move from room (0, 0) to room (1, 0) in one second.
+At time t == 5, move from room (1, 0) to room (1, 1) in two seconds.
+Example 2:
+
+Input: moveTime = [[0,0,0,0],[0,0,0,0]]
+
+Output: 6
+
+Explanation:
+
+The minimum time required is 6 seconds.
+
+At time t == 0, move from room (0, 0) to room (1, 0) in one second.
+At time t == 1, move from room (1, 0) to room (1, 1) in two seconds.
+At time t == 3, move from room (1, 1) to room (1, 2) in one second.
+At time t == 4, move from room (1, 2) to room (1, 3) in two seconds.
+Example 3:
+
+Input: moveTime = [[0,1],[1,2]]
+
+Output: 4
+
+ 
+
+Constraints:
+
+2 <= n == moveTime.length <= 750
+2 <= m == moveTime[i].length <= 750
+0 <= moveTime[i][j] <= 109
+'''
+
+
+
+
+
+import heapq
+
+class Solution:
+    def minTimeToReach(self, moveTime):
+        n, m = len(moveTime), len(moveTime[0])
+        vis = [[False] * m for _ in range(n)]
+        heap = [(0, 0, 0, 0)] 
+        vis[0][0] = True
+        dirs = [(0,1), (1,0), (-1,0), (0,-1)]
+
+        while heap:
+            time, moves, r, c = heapq.heappop(heap)
+            if r == n - 1 and c == m - 1:
+                return time
+            for dr, dc in dirs:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < n and 0 <= nc < m and not vis[nr][nc]:
+                    vis[nr][nc] = True
+                    wait = max(time, moveTime[nr][nc])
+                    travel_time = 1 if moves % 2 == 0 else 2
+                    heapq.heappush(heap, (wait + travel_time, moves + 1, nr, nc))
+        return -1
