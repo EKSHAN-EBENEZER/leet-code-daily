b4a580b156f76877942f565186e0d0ed43b52a66
Ekshan Ebenezer <69078104+EKSHAN-EBENEZER@users.noreply.github.com>
2025-05-25 21:51:04 +0530

    Create 2131. Longest Palindrome by Concatenating Two Letter Words.py

---
+++ 2131. Longest Palindrome by Concatenating Two Letter Words.py
@@ -0,0 +1,58 @@
+'''
+You are given an array of strings words. Each element of words consists of two lowercase English letters.
+
+Create the longest possible palindrome by selecting some elements from words and concatenating them in any order. Each element can be selected at most once.
+
+Return the length of the longest palindrome that you can create. If it is impossible to create any palindrome, return 0.
+
+A palindrome is a string that reads the same forward and backward.
+
+ 
+
+Example 1:
+
+Input: words = ["lc","cl","gg"]
+Output: 6
+Explanation: One longest palindrome is "lc" + "gg" + "cl" = "lcggcl", of length 6.
+Note that "clgglc" is another longest palindrome that can be created.
+Example 2:
+
+Input: words = ["ab","ty","yt","lc","cl","ab"]
+Output: 8
+Explanation: One longest palindrome is "ty" + "lc" + "cl" + "yt" = "tylcclyt", of length 8.
+Note that "lcyttycl" is another longest palindrome that can be created.
+Example 3:
+
+Input: words = ["cc","ll","xx"]
+Output: 2
+Explanation: One longest palindrome is "cc", of length 2.
+Note that "ll" is another longest palindrome that can be created, and so is "xx".
+ 
+
+Constraints:
+
+1 <= words.length <= 105
+words[i].length == 2
+words[i] consists of lowercase English letters.
+'''
+
+
+
+
+
+
+
+
+class Solution:
+    def longestPalindrome(self, words: List[str]) -> int:
+        freq=Counter(words)
+        pal=0
+        hasDouble=False
+        for key, val in freq.items():
+            if key[0]==key[1]:
+                pal+=val//2*4
+                if val&1: hasDouble=True
+            elif key[0]<key[1]:
+                rev=key[1]+key[0]
+                pal+=min(val, freq[rev] )*4
+        return pal+hasDouble*2
