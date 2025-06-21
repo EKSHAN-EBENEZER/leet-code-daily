0f8765786bb2378e2890db617de62ceb1f891778
Ekshan Ebenezer <69078104+EKSHAN-EBENEZER@users.noreply.github.com>
2025-06-21 23:11:34 +0530

    Create 3085. Minimum Deletions to Make String K-Special.py

---
+++ 3085. Minimum Deletions to Make String K-Special.py
@@ -0,0 +1,76 @@
+'''
+You are given a string word and an integer k.
+
+We consider word to be k-special if |freq(word[i]) - freq(word[j])| <= k for all indices i and j in the string.
+
+Here, freq(x) denotes the frequency of the character x in word, and |y| denotes the absolute value of y.
+
+Return the minimum number of characters you need to delete to make word k-special.
+
+ 
+
+Example 1:
+
+Input: word = "aabcaba", k = 0
+
+Output: 3
+
+Explanation: We can make word 0-special by deleting 2 occurrences of "a" and 1 occurrence of "c". Therefore, word becomes equal to "baba" where freq('a') == freq('b') == 2.
+
+Example 2:
+
+Input: word = "dabdcbdcdcd", k = 2
+
+Output: 2
+
+Explanation: We can make word 2-special by deleting 1 occurrence of "a" and 1 occurrence of "d". Therefore, word becomes equal to "bdcbdcdcd" where freq('b') == 2, freq('c') == 3, and freq('d') == 4.
+
+Example 3:
+
+Input: word = "aaabaaa", k = 2
+
+Output: 1
+
+Explanation: We can make word 2-special by deleting 1 occurrence of "b". Therefore, word becomes equal to "aaaaaa" where each letter's frequency is now uniformly 6.
+
+ 
+
+Constraints:
+
+1 <= word.length <= 105
+0 <= k <= 105
+word consists only of lowercase English letters.
+'''
+
+
+
+
+
+
+
+
+class Solution:
+    def minimumDeletions(self, word: str, k: int) -> int:
+        freqMap = defaultdict(int)
+        for c in word:
+            freqMap[c] += 1
+        
+        frequencies = sorted(freqMap.values())
+        minDeletions = float('inf')
+        n = len(frequencies)
+        
+        for i in range(n):
+            base = frequencies[i]
+            totalDeletions = 0
+            
+            for j in range(i):
+                totalDeletions += frequencies[j]
+            
+            for j in range(i, n):
+                if frequencies[j] > base + k:
+                    totalDeletions += frequencies[j] - (base + k)
+            
+            if totalDeletions < minDeletions:
+                minDeletions = totalDeletions
+        
+        return minDeletions
